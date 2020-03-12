b1e803bccbfb5f88a7ec0acfe29e9c524682ed88
Eric Coffman <ericbrian@gmail.com>
2020-03-12 18:52:56 -0400

    Move modules into components folder.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const { addUser, removeUser, getUser, getUsersInRoom, getRoomNames } = require('./users');
+const { addUser, removeUser, getUser, getUsersInRoom, getRoomNames } = require('./components/users');
 
 const PORT = process.env.PORT || 5000;
 
