de5e00bc0bd019542dffc98c1c71fcbfd23b609c
Eric Coffman <ericbrian@gmail.com>
2020-03-13 16:33:35 -0400

    Create the Join page as Home.

--- server/router.js
+++ server/router.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const { getRoomNames } = require('./components/users');
+
 router.get('/', (req, res) => {
     res.send('Server is up and running.');
 });
+
+router.get('/api/getRoomNames', (req, res) => {
+    res.send(getRoomNames());
+})
 
 module.exports = router;
